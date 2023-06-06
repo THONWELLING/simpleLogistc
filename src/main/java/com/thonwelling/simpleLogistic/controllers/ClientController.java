baa3ea93831e14c851f31c3a663399d09f92dfe6
THONWELLING <thonwellingdani13@gmail.com>
2023-06-06 00:16:30 -0300

    Add Basic Validation With Jakarta Bean Validation

--- src/main/java/com/thonwelling/simpleLogistic/controllers/ClientController.java
+++ src/main/java/com/thonwelling/simpleLogistic/controllers/ClientController.java
@@ -2,13 +2,13 @@ package com.thonwelling.simpleLogistic.controllers;
 
 import com.thonwelling.simpleLogistic.models.Client;
 import com.thonwelling.simpleLogistic.repositories.ClientRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/clients/v1")
@@ -19,6 +19,7 @@ public class ClientController {
   public List<Client> getAllClients() {
     return repository.findAll();
   }
+
   @GetMapping("/{id}")
   public ResponseEntity<Client> getClientById(@PathVariable Long id) {
     return repository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
@@ -28,17 +29,20 @@ public class ClientController {
   public List<Client> getClientByName() {
     return repository.findByName("Margarethe Cheek");
   }
+
   @GetMapping("/partname")
   public List<Client> getClientByNameContaining() {
     return repository.findByNameContaining("ga");
   }
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public Client createClient(@RequestBody Client client) {
+  public Client createClient(@Valid @RequestBody Client client) {
     return repository.save(client);
   }
+
   @PutMapping("/{id}")
-  public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
+  public ResponseEntity<Client> updateClient(@PathVariable Long id, @Valid @RequestBody Client client) {
     if (!repository.existsById(id)) {
       return ResponseEntity.notFound().build();
     }
--- src/main/java/com/thonwelling/simpleLogistic/models/Client.java
+++ src/main/java/com/thonwelling/simpleLogistic/models/Client.java
@@ -1,6 +1,9 @@
 package com.thonwelling.simpleLogistic.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @AllArgsConstructor
@@ -16,8 +19,18 @@ public class Client {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @NotBlank
+  @Size(max = 60)
   private String name;
+
+  @NotBlank
+  @Size(max = 80)
+  @Email
   private String email;
+
+  @NotBlank
+  @Size(max = 18)
   @Column(name = "phone")
   private String phoneNumber;
 }
